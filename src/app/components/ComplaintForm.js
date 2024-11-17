1479b0a2796dc238fe774f934e2ce5f0562ab47a
webgaurav02 <gauravcodes123@gmail.com>
2024-11-18 00:31:30 +0530

    Add links to cards under HIV Testing section and other visual changes

--- src/app/components/ComplaintForm.js
+++ src/app/components/ComplaintForm.js
@@ -5,7 +5,7 @@ import Form from './form'
 
 const ComplaintForm = () => {
   return (
-    <div className=' md:px-10 px-5 py-10 bg-black text-white'>
+    <div className=' md:px-10 px-5 py-10 bg-[#260707] text-white'>
         <h3 className='md:text-5xl text-3xl text-white md:text-left text-center font-bold uppercase'>Complaint Form</h3>
         <p className='mb-5 md:text-left text-center'>Please fill out the form with your complaint. We will get back to you as soon as possible.</p>
         <Form></Form>
