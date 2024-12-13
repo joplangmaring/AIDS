54f71592ccc2c5a1b4f7ba988a3f5b79608e6a90
webgaurav02 <gauravcodes123@gmail.com>
2024-12-13 20:02:50 +0530

    Added admin authentication

---
+++ src/app/admin/dashboard/layout.js
@@ -0,0 +1,82 @@
+// src/app/dashboard/page.js
+'use client';
+
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import Link from 'next/link';
+
+//React icons
+import { HiMenuAlt3 } from "react-icons/hi";
+import { MdOutlineDashboard, MdEventNote } from "react-icons/md";
+import { AiOutlineUser } from "react-icons/ai";
+import { IoTicketSharp } from "react-icons/io5";
+
+
+// Components
+// import Header from '../components/Header';
+// import Sidebar from '../components/Sidebar';
+import Loading from '@/app/components/Loading';
+
+
+export default function DashboardPage({ children }) {
+
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
+  const router = useRouter();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+
+    //To verify admin jwt token using cookies
+    const verifyUser = async () => {
+      try {
+        const res = await fetch('/api/auth/verify', {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+          const data = await res.json();
+          setUser(data.user);
+          setLoading(false);
+        } else {
+          router.push('/admin/login');
+        }
+      } catch (error) {
+        console.error('Failed to verify token', error);
+        router.push('/admin/login');
+      }
+    };
+
+    verifyUser();
+
+
+    //For the dashboard template script
+    // const script = document.createElement('script');
+    // script.src = "https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js";
+    // script.defer = true;
+
+    // document.body.appendChild(script);
+
+    // return () => {
+    //   document.body.removeChild(script);
+    // };
+
+
+  }, [router]);
+
+
+  if (loading) {
+    return <Loading />
+  }
+
+
+
+
+
+  return (
+    <div className="dark bg-white h-screen w-screen">
+
+    </div>
+  )
+}
---
+++ src/app/admin/login/page.js
@@ -0,0 +1,114 @@
+// src/app/login/page.js
+'use client';
+
+// Images
+import logo from "@/assets/aidlogo.png";
+
+
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from "next/link";
+
+
+export default function LoginPage() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const router = useRouter();
+
+  useEffect(() => {
+    //To verify admin jwt token using cookies
+    const verifyUser = async () => {
+      try {
+        const res = await fetch('/api/auth/verify', {
+          method: 'GET',
+          headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (res.ok) {
+          router.push('/admin/dashboard');
+        } else {
+          router.push('/admin/login');
+        }
+      } catch (error) {
+        console.error('Failed to verify token', error);
+        router.push('/admin/login');
+      }
+    };
+    verifyUser();
+  }, [router]);
+
+
+
+
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const res = await fetch('/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
+
+    if (res.ok) {
+      router.push('/admin/dashboard');
+    } else {
+      alert('Invalid username or password');
+    }
+  }
+
+  return (
+    <section className="dark">
+      <div className="flex flex-col items-center lg:justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+          <Image
+            className="m-5"
+            src={logo}
+            width={150}
+            height="auto"
+            alt="MACS logo"
+          />
+        <div className="w-full bg-[#8b0000] rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className=" text-xl font-bold leading-tight tracking-tight text-center uppercase text-white">
+              Admin Login
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-white">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className=" border border-gray-300 text-gray-900 sm:text-sm rounded-lg block w-full p-2"
+                  placeholder="username"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-[#00FF38] focus:border-[#00FF38] block w-full p-2.5 "
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                </div>
+              </div>
+              <button type="submit" className="w-full text-black dark:bg-green-500 bg-green-500 focus:ring-4 focus:outline-none focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
